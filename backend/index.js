792481f7467dbb05021edf2d3e468b036ece517b
Christy C <57055910+christyc01@users.noreply.github.com>
2024-04-23 16:59:20 -0600

    Add root route

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,11 @@ import { PORT } from './config.js';
 
 const app = express();
 
+app.get('/', (req, res) => {
+  console.log(req);
+  return res.status(234).send('Hello from root');
+});
+
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
