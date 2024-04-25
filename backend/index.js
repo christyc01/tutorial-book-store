cab93868cedfe25e4cf34a6736cbd0eaaaf29eaa
Christy C <57055910+christyc01@users.noreply.github.com>
2024-04-25 10:41:26 -0600

    Connect to MongoDB through Mongoose

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,20 @@
 import express from 'express';
-import { PORT } from './config.js';
+import mongoose from 'mongoose';
+import { PORT, mongoDBURL } from './config.js';
 
 const app = express();
+
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log('Connected through Mongoose');
+    app.listen(PORT, () => {
+      console.log(`Listening on port ${PORT}`);
+    });
+  })
+  .catch((error) => console.log(error));
 
 app.get('/', (req, res) => {
   console.log(req);
   return res.status(234).send('Hello from root');
-});
-
-app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
 });
