06d3b855dcac5fc0a0bcce6f7e3fa0a85c5aeb5f
Christy C <57055910+christyc01@users.noreply.github.com>
2024-04-29 15:53:22 -0600

    Fix client port number

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: 'http:localhost:3000',
+    origin: 'http://localhost:5174',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type'],
   })
