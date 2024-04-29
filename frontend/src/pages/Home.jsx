98503de5897fe32103185e321601090596d0405c
Christy C <57055910+christyc01@users.noreply.github.com>
2024-04-29 15:54:19 -0600

    Add rough loading spinner and basic book data to home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,43 @@
+import { useEffect, useState } from 'react';
+import Spinner from '../components/Spinner';
+
 const Home = () => {
-  return <h1>Home</h1>;
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      // Example using try/catch
+      // try {
+      //   const response = await fetch('http://localhost:5555/books');
+      //   const parsedResponse = await response.json();
+      //   setData(parsedResponse);
+      // } catch (error) {
+      //   console.error('Failed to fetch:', error);
+      // }
+
+      // Example using .then()
+      fetch('http://localhost:5555/books')
+        .then((res) => res.json())
+        .then((parsedRes) => setData(parsedRes));
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      {!data.length ? (
+        <Spinner />
+      ) : (
+        <div>
+          <h1>Home</h1>
+          <p>Books list</p>
+          {data?.data?.map((item) => (
+            <p key={item._id}>{item.name}</p>
+          ))}
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Home;
