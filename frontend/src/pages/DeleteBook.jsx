d881a8ca3ec2c2afab9c4f423422d6571cfa8648
Christy C <57055910+christyc01@users.noreply.github.com>
2024-05-11 11:26:21 -0600

    Add loading state and spinner

--- frontend/src/pages/DeleteBook.jsx
+++ frontend/src/pages/DeleteBook.jsx
@@ -1,26 +1,33 @@
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import Spinner from '../components/Spinner';
 
 const DeleteBook = () => {
   const { id } = useParams();
   const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
 
   const handleDelete = () => {
     console.log('deleting', id);
+    setLoading(true);
     axios
       .delete(`http://localhost:5555/books/${id}`)
       .then(() => {
         navigate('/');
+        setLoading(false);
       })
       .catch((error) => {
         alert('An error occurred - check the console.');
         console.log(error);
+        setLoading(false);
       });
   };
 
   return (
     <div>
+      {loading ? <Spinner /> : ''}
       <h1>Delete Book</h1>
       <button onClick={handleDelete}>Delete</button>
     </div>
