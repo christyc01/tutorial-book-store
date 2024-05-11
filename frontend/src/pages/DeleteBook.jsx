8516450738eb055013827269117a53e2ccc42750
Christy C <57055910+christyc01@users.noreply.github.com>
2024-05-11 10:48:56 -0600

    Add basic deleting

--- frontend/src/pages/DeleteBook.jsx
+++ frontend/src/pages/DeleteBook.jsx
@@ -1,5 +1,23 @@
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+
 const DeleteBook = () => {
-  return <h1>Delete Book</h1>;
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+  const handleDelete = () => {
+    console.log('deleting', id);
+    axios.delete(`http://localhost:5555/books/${id}`);
+    navigate('/');
+  };
+
+  return (
+    <div>
+      <h1>Delete Book</h1>
+      <button onClick={handleDelete}>Delete</button>
+    </div>
+  );
 };
 
 export default DeleteBook;
