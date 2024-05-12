d436deccc73a08af2eef48d88d9626d14a7a480e
Christy C <57055910+christyc01@users.noreply.github.com>
2024-05-11 20:16:36 -0600

    Add details/edit/delete buttons to cards

--- frontend/src/components/home/BooksCard.jsx
+++ frontend/src/components/home/BooksCard.jsx
@@ -1,5 +1,10 @@
 /* eslint-disable react/prop-types */
 
+import { AiOutlineEdit } from 'react-icons/ai';
+import { BsInfoCircle } from 'react-icons/bs';
+import { MdOutlineDelete } from 'react-icons/md';
+import { Link } from 'react-router-dom';
+
 const BooksCard = ({ data }) => {
   return (
     <div>
@@ -9,11 +14,17 @@ const BooksCard = ({ data }) => {
           <p>title: {book.title}</p>
           <p>author: {book.author}</p>
           <p>year: {book.publishYear}</p>
+          <Link to={`/books/details/${book._id}`}>
+            <BsInfoCircle className="text-2xl text-green-800" />
+          </Link>
+          <Link to={`/books/edit/${book._id}`}>
+            <AiOutlineEdit className="text-2xl text-yellow-600" />
+          </Link>
+          <Link to={`/books/delete/${book._id}`}>
+            <MdOutlineDelete className="text-2xl text-red-600" />
+          </Link>
         </div>
       ))}
-      <p>details</p>
-      <p>edit</p>
-      <p>delete</p>
     </div>
   );
 };
