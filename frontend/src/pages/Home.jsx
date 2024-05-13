63f29dcf6eee9e1d25f5f4da86138802d940f400
Christy C <57055910+christyc01@users.noreply.github.com>
2024-05-13 12:17:07 -0600

    Update styling

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -45,22 +45,22 @@ const Home = () => {
 
   return (
     <div className="p-4">
+      <div className="flex justify-center items-center gap-x-4">
+        <button
+          onClick={() => setView('table')}
+          className="bg-sky-300 hover:bg-sky-600 px-4 py-1 rounded-lg"
+        >
+          Table
+        </button>
+        <button
+          onClick={() => setView('card')}
+          className="bg-sky-300 hover:bg-sky-600 px-4 py-1 rounded-lg"
+        >
+          Card
+        </button>
+      </div>
       <div className="flex justify-between items-center">
         <h1 className="text-3xl my-8">Books List</h1>
-        <div className="flex gap-4">
-          <button
-            onClick={() => setView('table')}
-            className="p-6 bg-red-300 rounded-lg"
-          >
-            Table
-          </button>
-          <button
-            onClick={() => setView('card')}
-            className="p-6 bg-blue-300 rounded-lg"
-          >
-            Card
-          </button>
-        </div>
         <Link to="/books/create">
           <MdOutlineAddBox className="text-sky-800 text-4xl" />
         </Link>
