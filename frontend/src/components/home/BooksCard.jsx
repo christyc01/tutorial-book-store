707e7959ed442f346a375953d82c9dadaa74ca47
Christy C <57055910+christyc01@users.noreply.github.com>
2024-05-13 14:22:28 -0600

    More styling

--- frontend/src/components/home/BooksCard.jsx
+++ frontend/src/components/home/BooksCard.jsx
@@ -1,28 +1,43 @@
 /* eslint-disable react/prop-types */
 
 import { AiOutlineEdit } from 'react-icons/ai';
+import { PiBookOpenTextLight } from 'react-icons/pi';
+import { BiUserCircle } from 'react-icons/bi';
 import { BsInfoCircle } from 'react-icons/bs';
 import { MdOutlineDelete } from 'react-icons/md';
 import { Link } from 'react-router-dom';
 
 const BooksCard = ({ data }) => {
   return (
-    <div>
+    <div className="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
       {data.map((book) => (
-        <div key={book._id} className="border-2 rounded-sm">
-          <p>id: {book._id}</p>
-          <p>title: {book.title}</p>
-          <p>author: {book.author}</p>
-          <p>year: {book.publishYear}</p>
-          <Link to={`/books/details/${book._id}`}>
-            <BsInfoCircle className="text-2xl text-green-800" />
-          </Link>
-          <Link to={`/books/edit/${book._id}`}>
-            <AiOutlineEdit className="text-2xl text-yellow-600" />
-          </Link>
-          <Link to={`/books/delete/${book._id}`}>
-            <MdOutlineDelete className="text-2xl text-red-600" />
-          </Link>
+        <div
+          key={book._id}
+          className="border-2 border-gray-500 rounded-lg px-4 py-2 m-4 relative hover:shadow-xl"
+        >
+          <h2 className="absolute top-1 right-2 px-4 py-1 bg-red-300 rounded-lg">
+            {book.publishYear}
+          </h2>
+          <h4 className="my-2 text-gray-500">{book._id}</h4>
+          <div className="flex justify-start items-center gap-x-2">
+            <PiBookOpenTextLight className="text-red-300 text-2xl" />
+            <h2 className="my-1">{book.title}</h2>
+          </div>
+          <div className="flex justify-start items-center gap-x-2">
+            <BiUserCircle className="text-red-300 text-2xl" />
+            <h2 className="my-1">{book.author}</h2>
+          </div>
+          <div className="flex justify-between items-center gap-x-2 mt-4 p-4">
+            <Link to={`/books/details/${book._id}`}>
+              <BsInfoCircle className="text-2xl text-green-800" />
+            </Link>
+            <Link to={`/books/edit/${book._id}`}>
+              <AiOutlineEdit className="text-2xl text-yellow-600" />
+            </Link>
+            <Link to={`/books/delete/${book._id}`}>
+              <MdOutlineDelete className="text-2xl text-red-600" />
+            </Link>
+          </div>
         </div>
       ))}
     </div>
